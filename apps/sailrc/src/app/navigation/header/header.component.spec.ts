c2dccb802440b7fa33ba8a99939b4e348a5c1a28
Zsolt Zsuffa <zsolt.zsuffa@gmail.com>
2025-04-03 13:16:25 +0200

    doc(initial setup): Reorganise HeaderComponent tests.

--- apps/sailrc/src/app/navigation/header/header.component.spec.ts
+++ apps/sailrc/src/app/navigation/header/header.component.spec.ts
@@ -1,42 +1,29 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { HeaderComponent } from './header.component';
 import { fireEvent, render, screen } from '@testing-library/angular';
-import { By } from '@angular/platform-browser';
 
 describe('HeaderComponent', () => {
-  let component: HeaderComponent;
-  let fixture: ComponentFixture<HeaderComponent>;
-
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [HeaderComponent],
-    }).compileComponents();
-
-    fixture = TestBed.createComponent(HeaderComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
-
   describe('Verify component structure', () => {
-    it('Should create component', () => {
-      expect(component).toBeTruthy();
+    it('Should create component', async () => {
+      const { fixture } = await render(HeaderComponent);
+      expect(fixture.componentInstance).toBeTruthy();
     });
 
-    it('template structure contains: mat-toolbar:', () => {
-      const matToolbar = fixture.debugElement.query(By.css('mat-toolbar')).nativeElement;
+    it('template structure contains: mat-toolbar:', async () => {
+      await render(HeaderComponent);
+      const matToolbar = screen.getByTestId('header-toolbar');
       expect(matToolbar).toBeTruthy();
     });
   });
-});
 
-describe('Verify DOM event handling', () => {
-  it('Click on menu button emits an undefined event', async () => {
-    const toggleSideNavSpy = jest.fn();
-    await render( HeaderComponent, { on: {
-      toggleSideNav: toggleSideNavSpy,
-    }});
-    const menuButton = screen.getByTestId('sidenav-toggle');
-    fireEvent.click(menuButton);
-    expect(toggleSideNavSpy).toHaveBeenCalled();
+  describe('Verify DOM event handling', () => {
+    it('Click on menu button emits an undefined event', async () => {
+      const toggleSideNavSpy = jest.fn();
+      await render( HeaderComponent, { on: {
+          toggleSideNav: toggleSideNavSpy,
+        }});
+      const menuButton = screen.getByTestId('sidenav-toggle');
+      fireEvent.click(menuButton);
+      expect(toggleSideNavSpy).toHaveBeenCalled();
+    })
   })
-})
+});
